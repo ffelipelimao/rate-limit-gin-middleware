35b3860e4be3694a8a0eff4f15f963d873ce19f6
Luiz Felipe Limao <lflimao@hotmail.com>
2024-01-20 17:58:57 -0300

    finished rate limit gin go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/ffelipelimao/rate-limit-gin-middleware/pkg/ratelimit"
 	"github.com/gin-gonic/gin"
@@ -11,12 +12,13 @@ func main() {
 
 	app := gin.Default()
 
-	rateLimit := ratelimit.NewRateLimit(5, 5)
+	rateLimit := ratelimit.NewRateLimit(5, 5*time.Second)
 
 	app.Use(rateLimit.Apply())
 
 	app.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "ok"})
+		return
 	})
 
 	app.Run()
--- pkg/ratelimit/ratelimit.go
+++ pkg/ratelimit/ratelimit.go
@@ -1,6 +1,7 @@
 package ratelimit
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,22 +10,59 @@ import (
 type RateLimit struct {
 	Requests int
 	Interval time.Duration
-	Storage  map[string]int
+	Storage  map[string]storage
+}
+
+type storage struct {
+	Value     int
+	Timestamp time.Time
 }
 
 func NewRateLimit(req int, interval time.Duration) *RateLimit {
-	return &RateLimit{
+	rl := &RateLimit{
 		Requests: req,
 		Interval: interval,
-		Storage:  make(map[string]int),
+		Storage:  make(map[string]storage),
 	}
+
+	go rl.purge()
+
+	return rl
 }
 
 func (r *RateLimit) Apply() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//Map ip-timestamp: numero de requeets
+		IP := c.ClientIP()
 
+		userRequests, ok := r.Storage[IP]
+		if !ok {
+			r.Storage[IP] = storage{
+				Value:     1,
+				Timestamp: time.Now(),
+			}
+			c.Next()
+			return
+		}
+		userRequests.Value++
+		r.Storage[IP] = userRequests
+
+		if userRequests.Value >= r.Requests {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "rate limit quota"})
+			c.Abort()
+			return
+		}
 	}
 }
 
-func (r *RateLimit) purge() {}
+func (r *RateLimit) purge() {
+	for {
+		time.Sleep(r.Interval)
+		currentTime := time.Now()
+
+		for key, s := range r.Storage {
+			if currentTime.Sub(s.Timestamp) > r.Interval {
+				delete(r.Storage, key)
+			}
+		}
+	}
+}
